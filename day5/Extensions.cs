1fbaf48d63c4309795c403564a8045c2cc0a4333
rob t <bobt_37@hotmail.com>
2023-12-06 09:06:30 +0000

    Day 6

--- day5/Extensions.cs
+++
@@ -1,8 +0,0 @@
-namespace day5;
-public static class Extensions
-{
-    public static T GetValueOrDefault<T>(this Dictionary<T,T> dictionary, T key)
-    {
-        return dictionary.TryGetValue(key, out T outValue) ? outValue : key;
-    }
-}
---
+++ day6/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//List<(int time, int distance)> input = new() {(7,9),(15,40),(30,200)}; //test
+List<(double time, double distance)> input = new() {(54,302),(94,1476),(65,1029),(92,1404)};
+
+var part1 = input
+    .Select(i => CalcWinningCombinations(i.time, i.distance))
+    .Aggregate((a,x) => a*x);
+
+Console.WriteLine($"Part 1 = {part1}");
+
+//var part2 = CalcWinningCombinations(71530, 940200); //test
+var part2 = CalcWinningCombinations(54946592, 302147610291404);
+Console.WriteLine($"Part 2 = {part2}");
+
+double CalcWinningCombinations(double time, double distance)
+{
+    double minX = 0;
+    double maxX = time;
+    for(double i = 0; i<time; i++)
+    {
+        var computedDistance = i*(time-i);
+        if(computedDistance > distance && minX == 0)
+            minX = i;
+        
+        if(computedDistance <= distance && minX > 0)
+        {
+            maxX = i-1;
+            break;
+        }
+    }
+    return maxX-minX+1;
+}
